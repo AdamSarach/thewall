b26e3c622fa0e4fc22f08c88dd7f36efe8edc6bf
Adam <adamsarach@gmail.com>
2020-11-30 19:57:33 +0100

    fetch for wnp and money completed

--- backend/soup.py
+++ backend/soup.py
@@ -17,26 +17,63 @@ def get_data_to_read():
     return data
 
 
-def extract_data(text):
+# def extract_data(text):
 
-    bankier_list = []
-    soup = BeautifulSoup(text, 'html.parser')
-    section = soup.find("div", {"class": "o-home-dailynews-box__list"})
-    for link in section.find_all('a', {"class": "m-title-with-label-item"}):
-        class_list = link.get_attribute_list('class')
-        if len(class_list) > 1:
-            continue
+    # money_list = []
+    # money_domain = "https://www.money.pl/"
+    # soup = BeautifulSoup(text, 'html.parser')
+    # ul_element = soup.find("ul", {"class": "w4z002-3 bzICKL"})
+    # for link in ul_element.find_all('li'):
+    #     inner_link = link.find("a")
+    #     description = inner_link['title']
+    #     path = inner_link['href']
+    #     url = money_domain + path
+    #     list_element = {
+    #         "url": url,
+    #         "description": description
+    #     }
+    #     money_list.append(list_element)
+    # return money_list
 
-        url = link['data-vr-contentbox-url']
-        image = link.find("img")
-        description = image["alt"]
-        list_element = {
-            "url": url,
-            "description": description
-        }
-        bankier_list.append(list_element)
-
-    return bankier_list
+# extract_data(get_data_to_read())
 
 
-extract_data(get_data_to_read())
+# def extract_wnp_data(text):
+
+    # wnp_list = []
+    # soup = BeautifulSoup(text, 'html.parser')
+    # tabs = soup.find("div", {"class": "tabs"})
+    # news = tabs.find("div", {"class": "one active"})
+    # for link in news.find_all('a'):
+    #     url = link['href']
+    #     description = link.find("h3").text
+    #     list_element = {
+    #         "url": url,
+    #         "description": description
+    #     }
+    #     wnp_list.append(list_element)
+    # return wnp_list
+
+# def extract_bankier_data(text):
+#
+#     bankier_list = []
+#     soup = BeautifulSoup(text, 'html.parser')
+#     section = soup.find("div", {"class": "o-home-dailynews-box__list"})
+#     for link in section.find_all('a', {"class": "m-title-with-label-item"}):
+#         class_list = link.get_attribute_list('class')
+#         if len(class_list) > 1:
+#             continue
+#
+#         url = link['data-vr-contentbox-url']
+#         image = link.find("img")
+#         description = image["alt"]
+#         list_element = {
+#             "url": url,
+#             "description": description
+#         }
+#         bankier_list.append(list_element)
+#
+#     return bankier_list
+
+
+
