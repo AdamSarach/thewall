ec5f9128b6ea685f5e342aec682e4440c2f22932
Adam <adamsarach@gmail.com>
2020-12-10 20:05:14 +0100

    Fixed layout and readme file

--- project/settings.py
+++ project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = SECRET_KEY = config('KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
